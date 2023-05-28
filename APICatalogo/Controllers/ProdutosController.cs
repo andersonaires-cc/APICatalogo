5c639d34fadcaac282d085ed48e030cc7c8cbcb7
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2023-05-27 22:20:44 -0300

    method Get for Id

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -3,31 +3,43 @@ using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace APICatalogo.Controllers
+namespace APICatalogo;
+
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ProdutosController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
-
-        public ProdutosController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
-        {
-            var produtos = _context.Produtos.ToList();
-            if(produtos is null)
-            {
-                //equivalente ao 404
-                //bad request
-                return NotFound("Produtos não encontrados...");
-            }
-
-            return produtos;
-        }
+        _context = context;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+        if (produtos is null)
+        {
+        //equivalente ao 404
+        //bad request
+        return NotFound("Produtos não encontrados...");
+        }
+
+        return produtos;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if(produto is null)
+        {
+            return NotFound("Produto não encontrado...");
+        }
+        return produto;
+    } 
 }
+
