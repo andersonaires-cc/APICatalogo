4a64b0a595bf2f1416d52506fa640014779956e1
anderson <andersonairesspp@hotmail.com>
2023-09-10 11:07:58 -0300

    Validation - II

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
@@ -28,5 +28,26 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if(primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new 
+                    ValidationResult("A primeira letra do produto deve ser maiúscula",
+                    new[] {nameof(this.Nome)});
+            }
+        }
+
+        if(this.Estoque <= 0)
+        {
+            yield return new
+                    ValidationResult("O estoque deve ser maior que zero",
+                    new[] { nameof(this.Estoque) });
+        }
+    }
 }
 
