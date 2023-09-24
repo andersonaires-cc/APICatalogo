78e554a5ab3a7d10c184e538ea6b7bc4e33c66c3
anderson <andersonairesspp@hotmail.com>
2023-09-24 13:56:42 -0300

    DTO and AutoMapper

---
+++ APICatalogo/DTOs/ProdutoDTO.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Validations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace APICatalogo.DTOs
+{
+    public class ProdutoDTO
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
